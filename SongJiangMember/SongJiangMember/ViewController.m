843e4aa463a094eac4f781376368098ce5052992
wangkai <594289156@qq.com>
2015-05-26 13:11:47 +0800

    [MOD] wangkai test

--- SongJiangMember/SongJiangMember/ViewController.m
+++ SongJiangMember/SongJiangMember/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-
 @interface ViewController ()
 
 @end
@@ -16,8 +15,61 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  
+  [self test];
+    
+    
+    
   // Do any additional setup after loading the view, typically from a nib.
 }
+
+
+-(void)test{
+    
+    UIEdgeInsets padding = UIEdgeInsetsMake(10, 10, 300, 10);
+    
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor yellowColor];
+    
+    [self.view addSubview:view];
+    
+    [view mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        //make.edges.equalTo(self.view).with.insets(padding);
+       // make
+        
+    }];
+    
+    [self performSelector:@selector(click:) withObject:view afterDelay:1.0];
+   // [self remark:view];
+    
+}
+
+-(void)click:(UIView*)view{
+    
+    
+    [self remark:view];
+    
+    
+}
+
+
+-(void)remark:(UIView*)view{
+    
+    [view mas_remakeConstraints:^(MASConstraintMaker *make) {
+        
+        //make.size.equalTo(self.view);
+        
+       
+            make.top.and.left.offset(10);
+        
+            make.bottom.and.right.offset(-10);
+        
+    }];
+    
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
